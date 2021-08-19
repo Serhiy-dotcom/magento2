aa5e5f2964953ee2e0e57cc6b26a41dd0f1607ff
Serhiy <dvoryannikovserhiy@gmail.com>
2021-08-19 13:14:23 +0300

    seems done, only variables to add

---
+++ app/design/frontend/Magento/storefront/web/js/accordion.js
@@ -0,0 +1,44 @@
+define([
+    'jquery',
+    'jquery/ui',
+    'collapsible',
+    'matchMedia',
+    'domReady!',
+], function($){
+    'use strict';
+
+    $.widget('mage.accordion', {
+        options:{
+            currentElement: "",
+            collapsibleOptions: {
+                active: [0],
+                collapsible: true,
+                openedState: "active",
+                multipleCollapsible: true,
+                animate: {
+                    easing: "easeOutCubic",
+                    duration: "1000",
+                },
+            },
+        },
+
+        _create: function() {
+            console.log('Ready');
+
+            var currentElement = $(this.element);
+
+            mediaCheck({
+                media: '(min-width: 767px)',
+                entry: function () {
+                    $(currentElement).collapsible( "disable" );
+                    $(currentElement).collapsible( "destroy" );
+                }.bind(this),
+                exit: function () {
+                    $(currentElement).collapsible(this.options.collapsibleOptions);
+                }.bind(this),
+            });
+        },
+    });
+
+    return $.mage.accordion;
+})

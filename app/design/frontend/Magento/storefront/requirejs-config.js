7abf0282979876dd7826fe26b3917d68b64f39e0
Serhiy <dvoryannikovserhiy@gmail.com>
2021-08-20 16:22:17 +0300

    safe commit

--- app/design/frontend/Magento/storefront/requirejs-config.js
+++ app/design/frontend/Magento/storefront/requirejs-config.js
@@ -1,5 +1,12 @@
 var config = {
     paths: {
+        slick: 'js/plugins/slick',
         'accordionPath': 'js/accordion',
+        'sliderPath': 'js/slider'
     },
+    shim: {
+        slick: {
+            deps: ['jquery']
+        }
+    }
 };
---
+++ app/design/frontend/Magento/storefront/web/js/slider.js
@@ -0,0 +1,29 @@
+define([
+    'jquery',
+    'jquery/ui',
+    'slick',
+    'domReady!'
+], function ($) {
+    'use strict';
+
+    $.widget('mage.slider', {
+        options: {
+            selector:"",
+            sliderOptions:{
+                infinite: true,
+                prevArrow: '<div class="slick-prev custom__prev ">Prev</div>',
+                nextArrow: '<div class="slick-next custom__next ">Next</div>'
+            }
+        },
+
+        _create: function () {
+            console.log(`Ready slider - ${this.options.selector}`);
+
+            $(this.options.selector).slick(this.options.sliderOptions);
+
+
+        },
+    });
+
+    return $.mage.slider;
+});
